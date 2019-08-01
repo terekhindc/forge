8f4f0b5e92c27090cb2a8c1cec0d30c1b04c4f28
vrcorpru <head@vrcorp.ru>
2019-08-01 15:06:00 +0700

    add url mongo database in azure

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://cosmosdbnwd:6uO7SBtmHVI89OZ363BfuYeiOl495f92TKLiUAesHDuDVkT8wq74uTTgLFO1NyiX51p83YxvcfD1VNK4r65iiQ%3D%3D@cosmosdbnwd.documents.azure.com:10255/?ssl=true',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
